74bcfd88c1202e35687f41d8a53d539adc691c34
sunilzs <sunylkumar18@gmail.com>
2016-07-11 04:32:40 -0400

    data validation

--- todo-api/server.js
+++ todo-api/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = 3000;
@@ -22,13 +23,7 @@ app.get('/todos', function (req, res) {
 //GET /todos/id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
-    var matchedTodo;
-
-    todos.forEach(function (todo) {
-        if(todo.id === todoId){
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.find(todos, {'id': todoId});
 
     if(matchedTodo){
         res.json(matchedTodo);
@@ -39,18 +34,53 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post('/todos', function (req, res) {
-    // var body = req.body;
+    //var body = req.body;
 
-    // body.id = todoNextId++;
-    // todos.push(body);
+    //Use _.pick to only pick description and completed
+    var body = _.pick(req.body, 'description', 'completed');
 
-    // console.log('todos' + body.description);
-    res.json(req.body);
-
-    console.log(req.body);
-
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+    
+    body.description = body.description.trim();
+    console.log('todo description' + body.description);
+    body.id = todoNextId++;
+    todos.push(body);
+    res.json(body);
 
 });
+
+//GET /todos/id
+// app.get('/todos/:id', function (req, res) {
+//     var todoId = parseInt(req.params.id);
+//     var matchedTodo;
+
+//     todos.forEach(function (todo) {
+//         if(todo.id === todoId){
+//             matchedTodo = todo;
+//         }
+//     });
+
+//     if(matchedTodo){
+//         res.json(matchedTodo);
+//     }else{
+//         res.status(404).send();
+//     }
+// });
+
+//POST /todos
+// app.post('/todos', function (req, res) {
+//     var body = req.body;
+
+//     body.id = todoNextId++;
+//     todos.push(body);
+
+//     console.log('todo description' + body.description);
+//     res.json(req.body);
+
+
+// });
 
 
 
