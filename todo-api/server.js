bb239f1def894cc0ee95ab6ebf24ba4905b39ece
sunilzs <sunylkumar18@gmail.com>
2016-07-11 02:51:55 -0400

    ToDo rest api - first commit

--- todo-api/server.js
+++ todo-api/server.js
@@ -1,11 +1,59 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
+
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
-    res.send('Todo Api Root');
+    res.send('TODO Api Root ');
 });
 
-app.listen(PORT, function () {
-    console.log('Express listening on port '+ PORT);
+//GET /todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
 });
+
+//GET /todos/id
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id);
+    var matchedTodo;
+
+    todos.forEach(function (todo) {
+        if(todo.id === todoId){
+            matchedTodo = todo;
+        }
+    });
+
+    if(matchedTodo){
+        res.json(matchedTodo);
+    }else{
+        res.status(404).send();
+    }
+});
+
+//POST /todos
+app.post('/todos', function (req, res) {
+    // var body = req.body;
+
+    // body.id = todoNextId++;
+    // todos.push(body);
+
+    // console.log('todos' + body.description);
+    res.json(req.body);
+
+    console.log(req.body);
+
+
+});
+
+
+
+app.listen(PORT, function () {
+    console.log('Express runnning on port: ' + PORT)
+})
