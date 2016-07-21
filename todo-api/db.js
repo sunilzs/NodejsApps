79098f2c591f47675cf9ba71e9658c2519e02532
sunilzs <sunylkumar18@gmail.com>
2016-07-20 22:08:58 -0400

    Hash and Salt User password

--- todo-api/db.js
+++ todo-api/db.js
@@ -7,7 +7,10 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.user = sequelize.import(__dirname + '/models/user.js');
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 
 module.exports = db;
--- todo-api/server.js
+++ todo-api/server.js
@@ -68,6 +68,17 @@ app.post('/todos', function (req, res) {
     });
 });
 
+app.post('/users', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function (user) {
+        res.json(user.toPublicJSON())
+    }, function (e) {
+        res.status(400).json(e);
+    });
+
+})
+
 //DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
@@ -116,7 +127,7 @@ app.put('/todos/:id', function (req, res) {
     });
 })
 
-
+//{force: true}
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log('Express runnning on port: ' + PORT)
