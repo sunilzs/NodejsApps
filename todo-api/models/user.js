3b1726f70c5e0ee5f735022056a7ac19eef11e74
sunilzs <sunylkumar18@gmail.com>
2016-07-21 01:15:54 -0400

    Add login route and authenticate class method

--- todo-api/models/user.js
+++ todo-api/models/user.js
@@ -2,7 +2,9 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('user', {
+    //return sequelize.define('user', {
+    var user = sequelize.define('user', {
+
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -39,6 +41,24 @@ module.exports = function (sequelize, DataTypes) {
                     }
                 }
             },
+            classMethods: {
+                authenticate: function (body) {
+                    return new Promise(function (resolve, reject) {
+                        user.findOne({
+                            where: {
+                                email: body.email
+                            }
+                        }).then(function (user) {
+                            if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+                                return reject();
+                            }
+                            return resolve(user);
+                        }, function (e) {
+                            return reject();
+                        });
+                    });
+                }
+            },
             instanceMethods: {
                 toPublicJSON: function () {
                     var json = this.toJSON();
@@ -46,4 +66,6 @@ module.exports = function (sequelize, DataTypes) {
                 }
             }
         });
+
+        return user;
 };
--- todo-api/server.js
+++ todo-api/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = 3000;
@@ -68,17 +69,6 @@ app.post('/todos', function (req, res) {
     });
 });
 
-app.post('/users', function (req, res) {
-    var body = _.pick(req.body, 'email', 'password');
-
-    db.user.create(body).then(function (user) {
-        res.json(user.toPublicJSON())
-    }, function (e) {
-        res.status(400).json(e);
-    });
-
-})
-
 //DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
@@ -125,6 +115,46 @@ app.put('/todos/:id', function (req, res) {
     }, function () {
         res.status(500).send();
     });
+})
+
+app.post('/users', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function (user) {
+        res.json(user.toPublicJSON())
+    }, function (e) {
+        res.status(400).json(e);
+    });
+
+})
+
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    if (typeof (body.email) !== 'string' && typeof (body.password) !== 'string') {
+        res.status(400).send();
+    }
+
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    }, function (e) {
+        res.status(401).send()
+    });
+
+    // db.user.findOne({
+    //     where: {
+    //         email: body.email
+    //     }
+    // }).then(function (user) {
+    //     //bcrypt.compareSync(body.password, user.password_hash)
+    //     if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+    //         return res.status(401).send();
+    //     }
+    //     res.json(user.toPublicJSON());
+    // }, function (e) {
+    //     res.status(500).send();
+    // });
+
 })
 
 //{force: true}
